1d18ae5eacbe486f6ddc1cf39b94cf93646626fa
danielaser <daniela.serrano.dev@gmail.com>
2025-01-04 21:11:34 -0500

    feat(develop): add reservation controller and update reservationService methods

--- src/main/java/com/restaurant/restaurant/management/services/ReservationService.java
+++ src/main/java/com/restaurant/restaurant/management/services/ReservationService.java
@@ -1,9 +1,12 @@
 package com.restaurant.restaurant.management.services;
 
-import com.restaurant.restaurant.management.repositories.OrderRepository;
+import com.restaurant.restaurant.management.models.Reservation;
 import com.restaurant.restaurant.management.repositories.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
@@ -15,8 +18,27 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
-//    public ReservationDto createReservation(ReservationDto reservationDto) {
-//        Reservation reservation = ReservationMapper.toEntity(reservationDto);
-//        return ReservationMapper.toDto(reservationRepository.save(reservation));
-//    }
+    public Reservation addReservation(Reservation reservation) {
+        return reservationRepository.save(reservation);
+    }
+
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    public Optional<Reservation> getReservation(Long id) {
+        return reservationRepository.findById(id);
+    }
+
+    public Reservation updateReservation(Long id, Reservation reservationUpdated) {
+        return reservationRepository.findById(id).map(existingReservation -> {
+            existingReservation.setDateTime(reservationUpdated.getDateTime());
+            existingReservation.setNumberOfPeople(reservationUpdated.getNumberOfPeople());
+            return reservationRepository.save(existingReservation);
+        }).orElseThrow(() -> new RuntimeException("Reserva con ID: " + id + " no encontrada"));
+    }
+
+    public void deleteReservation(Long id) {
+        reservationRepository.deleteById(id);
+    }
 }
